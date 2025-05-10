8f3ae033f0725220dd5b536b94557b22b9ccc48e
Will Griffiths <wgriffiths@teamworks.com>
2025-05-10 12:11:06 +1000

    Adjust TAPPING_TERM in Kyria keymap rules.mk from 150 to 100 for improved responsiveness.

--- keyboards/splitkb/kyria/rev1/base/keymaps/willgriffiths/rules.mk
+++ keyboards/splitkb/kyria/rev1/base/keymaps/willgriffiths/rules.mk
@@ -3,4 +3,4 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 MASTER_LEFT = yes
-TAPPING_TERM = 150
+TAPPING_TERM = 100
