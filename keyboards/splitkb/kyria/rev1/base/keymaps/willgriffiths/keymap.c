e4aafc7df584768cd26eb2b2ad4d9991b679b5fa
Will Griffiths <wgriffiths@teamworks.com>
2025-05-10 12:17:10 +1000

    Refactor Kyria keymap: replace MOD_LALT and MOD_LCTL with LALT and LCTL for improved clarity and consistency in modifier assignments.

--- keyboards/splitkb/kyria/rev1/base/keymaps/willgriffiths/keymap.c
+++ keyboards/splitkb/kyria/rev1/base/keymaps/willgriffiths/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
       KC_ESC,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_CCCV,   MOD_HYPR, KC_DEL, XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-           MOD_LALT(KC_LSFT), MOD_LCTL(KC_LSFT), LT(NUMPAD, KC_TAB), KC_LSFT, MT(MOD_LGUI, KC_ENT), MT(MOD_RGUI,KC_BSPC), LT(NAV, KC_SPC), LT(SYM,KC_ENT), KC_RCTL, KC_RALT
+           LALT(KC_LSFT), LCTL(KC_LSFT), LT(NUMPAD, KC_TAB), KC_LSFT, MT(MOD_LGUI, KC_ENT), MT(MOD_RGUI,KC_BSPC), LT(NAV, KC_SPC), LT(SYM,KC_ENT), KC_RCTL, KC_RALT
     ),
 /*
  * Lower Layer: Numpad, Media
